21a047fc8734223cb733e4f61e01d07590acda60
Prashant-Alit <prashantsawle@alittech.com>
2024-11-08 12:47:07 +0530

    added hooks

--- src/screens/about/About.js
+++ src/screens/about/About.js
@@ -1,9 +1,18 @@
+import { useState } from "react"
 
+import {Button} from "../../components/button/Button"
 
 export const About = () => {
+    const [value,setValue] = useState(0)
+
+    const handleClick = () => {
+        setValue(value+1)
+    }
     return (
         <div>
             <h1>About Page</h1>
+             <div>{value}</div>
+            <Button title="increase" handleClick={handleClick}/>
         </div>
     )
 }
